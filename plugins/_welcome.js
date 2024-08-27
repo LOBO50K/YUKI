1c863701a038217cce36ae29db03ae8f3895f79b
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-08-27 16:34:35 -0300

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,22 +3,22 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://tinyurl.com/ylgu47w3')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://tinyurl.com/2bhtm2h2')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
 
   if (chat.bienvenida && m.messageStubType == 27) {
-    let bienvenida = `┌─★ *${botname}* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+    let bienvenida = `┌─★ *${botname}* \n│✯ 𝙱𝙸𝙴𝙽𝚅𝙴𝙽𝙸𝙳𝙾 ✯\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  𝙱𝙸𝙴𝙽𝚅𝙴𝙽𝙸𝙳𝙾 𝙰\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
     
 await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
   }
   
   if (chat.bienvenida && m.messageStubType == 28) {
-    let bye = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Por put   └───────────────┈ ⳹`
+    let bye = `┌─★ *${botname}* \n│𖣔 𝙰𝙳𝙸𝙾𝚂 𖣔\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  𝚂𝙴 𝙵𝚄𝙴\n   │✑ 𝙹𝙰𝙼𝙰𝚂 𝚃𝙴 𝚀𝚄𝙸𝚂𝙸𝙼𝙾𝚂 𝙰𝙲𝙰   └───────────────┈ ⳹`
 await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo)
   }
   
   if (chat.bienvenida && m.messageStubType == 32) {
-    let kick = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Por put   └───────────────┈ ⳹`
+    let kick = `┌─★ *${botname}* \n│🃟 𝙰𝙳𝙸𝙾𝚂 🃟\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  𝚂𝙴 𝙵𝚄𝙴\n   │✑ 𝙿𝙾𝚁 𝙿𝚄𝚃   └───────────────┈ ⳹`
 await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo)
 }}
