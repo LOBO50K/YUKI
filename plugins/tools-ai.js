9e8c552f45681f0cb066c13556767c6f67e04638
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-09-03 22:04:37 -0300

    Update tools-ai.js

--- plugins/tools-ai.js
+++ plugins/tools-ai.js
@@ -1,15 +1,19 @@
-import Starlights from '@StarlightsTeam/Scraper'
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `*🚩 Ingrese su petición*\n*🪼 Ejemplo de uso:* ${usedPrefix + command} como hacer estrella de papel`, m, rcanal)
-await m.react('💬')
+const handler = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, 'Escribe un texto para hablar con chatgpt', m);
+
 try {
-let { msg } = await Starlights.openAi(text)
-await conn.reply(m.chat, msg, m, rcanal)
-} catch {
-}}
-handler.help = ['ai *<petición>*']
-handler.tags = ['tools']
-handler.command = /^(miku|ai|ia|chatgpt|gpt)$/i
-handler.register = true
+let msg = await conn.sendMessage(m.chat, {text: '*chatgpt esta escribiendo.....*'});
+
+let userid = conn.getName(m.sender) || 'default';
+let apiurl = `https://api.guruapi.tech/ai/gpt4?username=${userid}&query=hii${encodeURIComponent(text)}`;
+let result = await fetch(apiurl);
+let response = await result.json();
+
+await conn.relayMessage(m.chat, { protocolMessage: { key: msg.key, type: 14, editedMessage: { conversation: response.msg }}}, {});
+} catch {}}
+
+handler.command = handler.help = ["chatgpt","ia"];
+
 export default handler
